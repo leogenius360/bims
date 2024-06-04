2e6c59855bbb6ee4574f3da3c5d903a1acf3bf0d
DOMINIC MAABOBRA TUOLONG <tuolongdominic@gmail.com>
2024-06-04 00:47:24 +0000

    Updated user_profile dropdown

--- src/components/user_profile.tsx
+++ src/components/user_profile.tsx
@@ -1,64 +1,103 @@
-import { Avatar, Button, Dropdown, DropdownItem, DropdownMenu, DropdownSection, DropdownTrigger, User } from "@nextui-org/react"
-import { ThemeSwitch } from "./theme-switch"
+import {
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownSection,
+  DropdownTrigger,
+  User,
+} from "@nextui-org/react";
 
 export const UserProfile = () => {
-    return (
-        <Dropdown placement="bottom-end" offset={20} showArrow shadow="md" closeOnSelect={false}>
-            <DropdownTrigger>
-                <button className="flex gap-4 items-center mx-2" type="button" aria-label="Profile">
-                    <Avatar isBordered showFallback className="w-6 h-6 text-tiny"
-                        src='https://i.pravatar.cc/150?u=a04258114e29026702d' />
-                </button>
-            </DropdownTrigger>
+  return (
+    <Dropdown
+      placement="bottom-end"
+      offset={20}
+      showArrow
+      shadow="md"
+      closeOnSelect={false}
+    >
+      <DropdownTrigger>
+        <button
+          className="flex gap-4 items-center mx-2"
+          type="button"
+          aria-label="Profile"
+        >
+          <Avatar
+            isBordered
+            showFallback
+            className="bg-transparent w-6 h-6 text-tiny"
+            src="images/avatar-placeholder.jpg"
+          />
+        </button>
+      </DropdownTrigger>
 
-            <DropdownMenu variant="faded" aria-label="User profile">
-                <DropdownItem isReadOnly key="profile" className="h-14 gap-2">
-                    <User name="Junior Leo" description="jnrleo@geniustechspace.com"
-                        classNames={{
-                            name: "text-default-600",
-                            description: "text-default-500",
-                        }}
-                        avatarProps={{
-                            size: "sm",
-                            src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-                        }}
-                    />
-                </DropdownItem>
+      <DropdownMenu variant="faded" aria-label="User profile">
+        <DropdownItem
+          key="profile"
+          showDivider
+          className="border-default cursor-default data-[hover=true]:bg-transparent data-[selectable=true]:focus:bg-transparent"
+          classNames={{ base: "bg-orange" }}
+        >
+          <User
+            name="Junior Leo"
+            description="jnrleo@geniustechspace.com"
+            classNames={{
+              // base: "bg-danger p-4",
+              name: "font-semibold",
+              description: "text-default-500",
+            }}
+            avatarProps={{
+              size: "sm",
+              src: "images/avatar-placeholder.jpg",
+            }}
+          />
+        </DropdownItem>
 
-                <DropdownItem
-                    key="new"
-                    shortcut="⌘N"
-                    description="Create a new file"
-                // startContent={<AddNoteIcon className={iconClasses} />}
-                >
-                    New file
-                </DropdownItem>
+        <DropdownItem
+          key="settings"
+          shortcut="⌘S"
+          description="Update your account info"
+          //   startContent={}
+          classNames={{ title: "font-semibold" }}
+        >
+          Settings
+        </DropdownItem>
 
-                <DropdownItem
-                    key="copy"
-                    shortcut="⌘C"
-                    description="Copy the file link"
-                // startContent={<CopyDocumentIcon className={iconClasses} />}
-                >
-                    Copy link
-                </DropdownItem>
+        <DropdownItem
+          key="copy"
+          shortcut="⌘C"
+          //   description="Copy the file link"
+          // startContent={<CopyDocumentIcon className={iconClasses} />}
+          classNames={{ title: "font-semibold" }}
+        >
+          Activities
+        </DropdownItem>
 
-                <DropdownItem
-                    key="edit"
-                    shortcut="⌘⇧E"
-                    showDivider
-                    description="Allows you to edit the file"
-                // startContent={<EditDocumentIcon className={iconClasses} />}
-                >
-                    Edit file
-                </DropdownItem>
+        <DropdownItem
+          showDivider
+          key="notifications"
+          shortcut="⌘N"
+          // startContent={<EditDocumentIcon className={iconClasses} />}
+          classNames={{ title: "font-semibold" }}
+        >
+          Notifications
+        </DropdownItem>
 
-                <DropdownSection className="mb-0">
-                    <DropdownItem key="theme" isReadOnly className="cursor-default">
-                        <ThemeSwitch />
-                    </DropdownItem>
-                </DropdownSection>
-            </DropdownMenu>
-        </Dropdown>
-    )
-}
+        <DropdownSection className="mb-0">
+          <DropdownItem
+            color="danger"
+            variant="solid"
+            key="logout"
+            classNames={{
+              base: "bg-danger text-white",
+              title: "text-center font-semibold",
+            }}
+          >
+            Log out
+          </DropdownItem>
+        </DropdownSection>
+      </DropdownMenu>
+    </Dropdown>
+  );
+};
