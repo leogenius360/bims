9944a9f3bc9e4a1202ca2b709141a62ea9491736
DOMINIC MAABOBRA TUOLONG <tuolongdominic@gmail.com>
2024-09-08 13:16:57 +0000

    Contract
    commented out contract utility file for build to pass

--- src/lib/contracts/utils.ts
+++ src/lib/contracts/utils.ts
@@ -1,24 +1,23 @@
-// utils/contract.ts
+// // utils/contract.ts
 
-import { ethers } from 'ethers';
-import { InventoryManager } from '../types/InventoryManager'; // Import your contract interface
+// import { ethers } from 'ethers';
 
-const CONTRACT_ADDRESS = 'YOUR_CONTRACT_ADDRESS';
-const ABI = [
-    // Your contract's ABI here
-];
+// const CONTRACT_ADDRESS = 'YOUR_CONTRACT_ADDRESS';
+// const ABI: ethers.ContractInterface = [
+//     // Your contract's ABI here
+// ];
 
-let provider: ethers.providers.Web3Provider;
-let contract: InventoryManager;
+// let provider: ethers.providers.Web3Provider;
+// let contract;
 
-export function initializeContract() {
-    if (typeof window !== 'undefined' && window.ethereum) {
-        provider = new ethers.providers.Web3Provider(window.ethereum);
-        const signer = provider.getSigner();
-        contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer) as InventoryManager;
-    } else {
-        throw new Error('No Ethereum provider found');
-    }
-}
+// export function initializeContract() {
+//     if (typeof window !== 'undefined' && window.ethereum) {
+//         provider = new ethers.providers.Web3Provider(window.ethereum);
+//         const signer = provider.getSigner();
+//         contract = new ethers.Contract(CONTRACT_ADDRESS, ABI, signer);
+//     } else {
+//         throw new Error('No Ethereum provider found');
+//     }
+// }
 
-export { provider, contract };
+// export { provider, contract };
